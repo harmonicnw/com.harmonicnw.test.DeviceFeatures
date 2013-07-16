0705a5c2ed83b648725764dd76a718fda0a27a0b
Gage Choat <harmonicnw@gmail.com>
2013-07-16 11:40:53 -0700

    store and parse json variable

--- js/index.js
+++ js/index.js
@@ -21,6 +21,10 @@ function initApp() {
 	console.log("howdy.");
 	document.body.style.backgroundColor = "#eee";
 	
+	// store a variable
+	var myStoredVal = window.localStorage.getItem("myStoredVal");
+	document.getElementById("myInput").value = myStoredVal;
+	
 	document.getElementById("vibrateLink").onclick = function() {
 		navigator.notification.vibrate( 1000 );
 		document.body.style.backgroundColor = "#fff";
@@ -32,4 +36,46 @@ function initApp() {
 		document.body.style.backgroundColor = "#faf";
 		return false;
 	}
+	
+	document.getElementById("myButton").onclick = function() {
+		myInputVal = document.getElementById("myInput").value;
+		window.localStorage.setItem("myStoredVal", myInputVal);
+		console.log( "myInputVal stored: " + myInputVal );
+	}
+	
+	document.getElementById("myJSONButton").onclick = function() {
+		myJsonString = '{"array": [1,2,3],"boolean": true,"null": null,"number": 123,"object": {"a": "b","c": "d","e": "f"},"string": "Hello World"}';
+		document.getElementById("myInput").value = myJsonString;
+	}
+	
+	document.getElementById("JsonString").onclick = function() {
+		var returnVal = "String not found.";
+		if ( IsJsonString(document.getElementById("myInput").value) ) {
+			var myJsonVal = JSON.parse( document.getElementById("myInput").value );
+			if (myJsonVal.hasOwnProperty('string')) {
+				var returnVal = myJsonVal.string;
+			}
+		}
+		alert(returnVal);
+	}
+	
+	document.getElementById("JsonObjA").onclick = function() {
+		var returnVal = "Object A not found.";
+		if ( IsJsonString(document.getElementById("myInput").value) ) {
+			var myJsonVal = JSON.parse( document.getElementById("myInput").value );
+			if ( myJsonVal.hasOwnProperty('object') && myJsonVal.object.hasOwnProperty('a') ) {
+				var returnVal = myJsonVal.object.a;
+			}
+		}
+		alert(returnVal);
+	}
+}
+
+function IsJsonString(str) {
+    try {
+        JSON.parse(str);
+    } catch (e) {
+        return false;
+    }
+    return true;
 }
